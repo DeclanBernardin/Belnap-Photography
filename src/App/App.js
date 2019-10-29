695268343097db32c03c32beb7aa61bad7bf9c47
Declan Bernardin <dmjbernardin@yahoo.com>
2019-10-28 18:04:03 -0700

    added and organized files

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import MainPage from '../MainPage/MainPage'
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Belnap Photography</h1>
+      
     </div>
   );
 }
